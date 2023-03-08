bc52a059756eb2de333f2c3943d8aa6728816a8a
NorbertoVergara <114522250+NorbertoVergara@users.noreply.github.com>
2023-03-08 16:54:28 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import NavigationBar from './components/navbar/navbar';
+import Barner from './components/barner-principal/Barner';
+import TresTarjetas from "./components/tarjetas/TresTarjetas";
+import Preparacion from './components/preparacion/Preparacion';
+import Estadistica from './components/estadistica/Estadistica';
+import Experiencia from './components/experiencia/Experiencia';
+import Footer from './components/Footer/Footer';
+import Boletin from './components/boletin-noticias/Boletin';
+
+const App = () => {
+  return (
+    <div>
+      <NavigationBar />
+      <Barner />
+      <TresTarjetas />
+      <Preparacion />
+      <Estadistica />
+      <Experiencia />
+      <Boletin/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
